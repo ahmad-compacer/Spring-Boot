41b4ff6074624b7d1d261c80274d7bad40fb0b9f
akhalil <akhalil@LAP-7439.teleoffice.lan>
2021-06-29 14:36:03 +0200

    APIs (GET, POST and DELETE)

--- Bank/src/main/java/com/Bank/Bank/MainController.java
+++ Bank/src/main/java/com/Bank/Bank/MainController.java
@@ -1,31 +1,27 @@
 package com.Bank.Bank;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//import com.fasterxml.jackson.annotation.JsonFilter;
 
 @Controller 
-@RequestMapping(path="/accounts") 
+@CrossOrigin(origins = "*")
+@RequestMapping(path="/v1") 
 public class MainController {
   @Autowired
   private AccountsRepository accountsRepository;
 
-  // post Parameter
- /* @PostMapping(path="/add") 
-  public @ResponseBody String addNewKonto (@RequestParam String name
-      , @RequestParam String Iban, @RequestParam Long balance) {*/
   
 	  @PostMapping(
 			value = "/add", 
@@ -33,7 +29,7 @@ public class MainController {
       		produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	  
 	    public ResponseEntity<Accounts> addNewAccount(@RequestBody Accounts accountDetails ) {
-
+		  
 		 Accounts n = new Accounts();
 		 n.setName(accountDetails.getName());
 		 n.setIban(accountDetails.getIban());
@@ -41,18 +37,17 @@ public class MainController {
 
 		  accountsRepository.save(n);
 		 return new ResponseEntity<Accounts>(n, HttpStatus.OK);	  
-  }
+	  	 }
 
   
-  @GetMapping(path="/all")
-  public @ResponseBody Iterable<Accounts> getAllUsers() {
-
-    return accountsRepository.findAll();
-  }
- /* @DeleteMapping(path="/delete")
-  public ResponseEntity<?> delete(@RequestParam(value = "id") String name) {
-	  
-	return new ResponseEntity<>(HttpStatus.OK);
-	  
-  }*/
+		 @GetMapping(path="/accounts")
+		 public @ResponseBody Iterable<Accounts> getAllUsers() {
+		 return accountsRepository.findAll();
+		 }
+		 
+		 @DeleteMapping(path="/delete/{id}")
+		 public ResponseEntity<Void> delete(@PathVariable int id) {
+			 accountsRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		 }
 }
